c8b4e7e98d85b9b20a4fea2cb1a0fd9a478e51a0
Rogerio Alcantara <carpe@carpetecnologia.com.br>
2020-08-15 16:53:23 -0300

    correções

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-interface CountdownProps {targetDate: string }
-
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+const Countdown: StorefrontFunctionComponent = ({}) => {
 
   return <div>
-    <h1>{ targetDate }</h1>
+    <h1>Teste Countdow</h1>
   </div>
 }
 
@@ -14,14 +11,7 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: { 
-    targetDate:{
-      title:'Data final',
-      description:'Data final utilizada no contador',
-      type: 'string',
-      default: null,
-    }
-   }
-}
+  properties: {}
+  }
 
 export default Countdown
